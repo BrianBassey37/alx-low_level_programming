accc33ab1f24f07094ea60ac1ee1175c8a2b71ec
brianbassey37 <brianbassey37@gmail.com>
2022-07-05 11:10:28 -0700

    input io

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -12,6 +12,7 @@
 *
 * Return: The result of the addition.
 */
+{
 int add(int num1, int num2)
 return (num1 + num2);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 * Auth: Brian Bassey
 */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
 * times_table - Prints the 9 times table, starting with 0.
