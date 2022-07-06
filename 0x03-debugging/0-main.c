3b53429174b814862dc66f0ffa59bef04de8f481
brianbassey37 <brianbassey37@gmail.com>
2022-07-06 14:35:02 -0700

    mainc

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/* more headers goes there */ 
+
+/* betty style doc for function main goes there */
+/**
+* main - main function
+* No Description:
+* Return: 0
+*/
+int main(void)
+{                                                       int n; 
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */ 
+
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+A file test that the function positive_or_negative() gives the correct output when given a case of 0.
