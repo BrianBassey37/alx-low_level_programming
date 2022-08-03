a72d77480416726a8c86fe3ac17f3c01d48ed658
brianbassey37 <brianbassey37@gmail.com>
2022-08-03 06:21:22 -0700

    function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-init_dog.c
+ * Author: Brian Bassey
+ */
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: input array
+ * @size: size of array
+ * @cmp: comparison function
+ * Return: index of integer found in cmp
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0 && cmp && size && array)
+		for (i = 0; i <= size; i++)
+			if (cmp(array[i]))
+				return (i);
+	return (-1);
+
+}
