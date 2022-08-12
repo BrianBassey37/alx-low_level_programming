2b2ba9c68cf9b25b8eb87a2e2405212200748381
brianbassey37 <brianbassey37@gmail.com>
2022-08-12 08:23:06 -0700

    review

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,19 @@
-uth: Brian Bassey
- */
+/*
+ *  * File: 0-print_list.c
+ *   * Author: Brian Bassey
+ *    */
 
 #include "lists.h"
 #include <stdio.h>
 
- /**
-  *  * print_list - Prints all the elements of a list_t list.
-  *   * @h: The list_t list.
-  *    *
-  *     * Return: The number of nodes in h.
-  *      */
+/**
+ *  * print_list - Prints all the elements of a list_t list.
+ *   * @h: The list_t list.
+ *    *
+ *     * Return: The number of nodes in h.
+ *      */
 
- size_t print_list(const list_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,17 @@
 /*
  *  * File: 1-list_len.c
- *   * Auth: Brian Bassey
+ *   * Author: Brian Bassey
  *    */
 
 #include "lists.h"
 
 /**
- *  * list_len - Finds the number of elements in a linked list_t list.
- *   * @h: The linked list_t list.
- *    * Return: The number of elements in h.
- *     */
+ *  * list_len - Finds the number of elements in
+ *   *            a linked list_t list.
+ *    * @h: The linked list_t list.
+ *     *
+ *      * Return: The number of elements in h.
+ *       */
 size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
