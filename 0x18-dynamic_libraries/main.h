a98ca5b643d40cd4715cca368906092b8b609c63
Brian Bassey <brianbassey37@gmail.com>
2022-10-03 19:53:37 +0100

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * File: main.h
+ * Auth: Brian Bassey
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libmy.a
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,4 +29,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
